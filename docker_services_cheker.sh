710ac218afd9e827a9258cc0a2c0f061c94fcd56
Алексей <alexleskov2000@gmail.com>
2022-05-26 12:41:38 +0800

    Завершать скрипт проверки сервисов ocker  ошибкой

--- docker_services_cheker.sh
+++ docker_services_cheker.sh
@@ -6,13 +6,11 @@ docker-compose ps
 running="$(docker-compose ps --services --filter "status=running")"
 services="$(docker-compose ps --services)"
 
-echo "running $running"
-echo "services services"
-
 if [ "$running" != "$services" ]; then
     echo "Following services are not running:"
-    # Bash specific
+    # выводим список сервисов с ошибкой
     comm -13 <(sort <<<"$running") <(sort <<<"$services")
+    exit 1
 else
     echo "All services are running"
 fi
