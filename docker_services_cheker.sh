62044332febda509945339569b660dff0a09817e
Алексей <alexleskov2000@gmail.com>
2022-05-26 12:45:54 +0800

    Вывод логов при ошибке запуска сервиса

--- docker_services_cheker.sh
+++ docker_services_cheker.sh
@@ -10,6 +10,8 @@ if [ "$running" != "$services" ]; then
     echo "Following services are not running:"
     # выводим список сервисов с ошибкой
     comm -13 <(sort <<<"$running") <(sort <<<"$services")
+    # вывод всех логов
+    docker-compose logs
     exit 1
 else
     echo "All services are running"
