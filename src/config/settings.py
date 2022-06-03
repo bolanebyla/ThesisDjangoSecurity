632224eab399744a11bf80a780802ff22f5b198e
Алексей <alexleskov2000@gmail.com>
2022-06-03 12:03:02 +0800

    collectstatic

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import environ
@@ -104,6 +105,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 
