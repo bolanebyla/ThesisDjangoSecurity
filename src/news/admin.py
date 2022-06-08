359d79e043b14ef9fa6188c0c1d9a17dca6ee13b
Алексей <alexleskov2000@gmail.com>
2022-06-08 12:37:57 +0800

    Модель новостей

--- src/news/admin.py
+++ src/news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title',)
+    search_fields = ('id', 'title',)
+    list_filter = ('author',)
+    readonly_fields = ('creation_date', 'update_date')
