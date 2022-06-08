7b9781a182857d0b3470ee1687242c8ee2badbfd
Алексей <alexleskov2000@gmail.com>
2022-06-08 12:47:37 +0800

    Проверка, что пользователь имеет доступ к админке

--- src/config/settings.py
+++ src/config/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'users.middleware.RestrictStaffToAdminMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
